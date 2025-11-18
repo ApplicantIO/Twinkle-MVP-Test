21a9fbb406c5f00b83c04b42f340b94e7b886d89
Bekha <yup.bekha@gmail.com>
2025-11-18 15:34:36 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ import { Sidebar } from "@/components/layout/Sidebar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Twinkle - #SaviyaliKontent",
+  title: "Twinkle - #SaviyaliKontent =)",
   description: "Video sharing platform for creators",
 };
 
