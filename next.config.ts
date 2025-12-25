4645f454534fedc5d9a9e69d53bb260d976edccc
Bekha <yup.bekha@gmail.com>
2025-12-25 22:28:42 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,31 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     // Common external image hosts used in the app (Firebase / Google user photos, cloud storage, CDNs)
-    domains: [
-      "firebasestorage.googleapis.com",
-      "storage.googleapis.com",
-      "lh3.googleusercontent.com",
-      "yt3.ggpht.com",
-      "images.unsplash.com",
-      "res.cloudinary.com",
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'yt3.ggpht.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
     ],
   },
   reactStrictMode: true,
